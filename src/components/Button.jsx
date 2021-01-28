5dcc1b6ea0e997db4a889b84b8528e32a4601498
Liene Putniņa <liene.putnina20@gmail.com>
2021-01-28 11:54:57 +0200

    Button added to the Header

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,28 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from "react";
+import PropTypes from "prop-types";
 
-const Button = () => (
-  <button type="submit" className="btn">
-    Add
+const Button = ({ color, text, onClick }) => (
+  <button
+    onClick={onClick}
+    type="submit"
+    className="btn"
+    style={{ backgroundColor: color }}
+  >
+    {text}
   </button>
 );
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  // eslint-disable-next-line react/require-default-props
+  onClick: PropTypes.func,
+};
 
 export default Button;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title }) => (
-  <header className="header">
-    <h1>{title}</h1>
-    <Button />
-  </header>
-);
-
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Click");
+  };
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
+  );
+};
 // If you want, you can add a default prop for title e.g.
-// Then whenever you put the title prop somewhere, it will show the default name
+// Then, whenever you put the title prop somewhere, it will show the default name
 // Header.defaultProps = {
 //   title: "Task Tracker",
 // };
 
 // To avoid entering the wrong proptype, define the type of prop one should add
 // If the wrong proptype is added, an error will show on the console
-// If you have "isRequired", no default props is needed
+// If you have "isRequired", no default props is needed, it kind of acts as a default prop
 Header.propTypes = {
   title: PropTypes.string.isRequired,
 };
