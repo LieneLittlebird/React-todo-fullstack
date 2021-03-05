e4f0835e0154faba451b5719dfbd149922ac9f98
LieneLittlebird <liene.putnina20@gmail.com>
2021-03-05 12:16:15 +0200

    uncommennted code, tried to debug get request in express

--- Server/index.js
+++ Server/index.js
@@ -10,18 +10,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.post("/tasks", async (req, res) => {
-//   const client = await MongoClient.connect(MONGO_DB_API, {
-//     useUnifiedTopology: true,
-//   });
-//   const db = client.db("tasks");
-//   const postsCollection = db.collection("tasks_saved");
-//   await postsCollection.insertOne(req.body);
+app.post("/tasks", async (req, res) => {
+  const client = await MongoClient.connect(MONGO_DB_API, {
+    useUnifiedTopology: true,
+  });
+  const db = client.db("tasks");
+  const postsCollection = db.collection("tasks_saved");
+  await postsCollection.insertOne(req.body);
 
-//   client.close();
+  client.close();
 
-//   res.send(JSON.stringify("Task added"));
-// });
+  res.send(JSON.stringify("Task added"));
+});
 
 app.get("/tasks", async (req, res) => {
   console.log("Tried to get stuff");
@@ -29,50 +29,56 @@ app.get("/tasks", async (req, res) => {
     useUnifiedTopology: true,
   });
   const db = client.db("tasks");
+  // console.log(db);
   const postsCollection = db.collection("tasks_saved");
-  postsCollection.find({}).toArray((docs) => {
+  // console.log(postsCollection);
+  const queryResult = postsCollection.find();
+  console.log(queryResult);
+
+  queryResult.toArray((docs) => {
+    console.log(docs);
     client.close();
     res.send(docs);
   });
 });
 
-// app.delete("/tasks/:id", async (req, res) => {
-//   const { id } = req.params.id;
-//   const client = await MongoClient.connect(MONGO_DB_API, {
-//     useUnifiedTopology: true,
-//   });
-//   const db = client.db("tasks");
-//   const postsCollection = db.collection("tasks_saved");
-//   await postsCollection.deleteOne({ id });
+app.delete("/tasks/:id", async (req, res) => {
+  const { id } = req.params.id;
+  const client = await MongoClient.connect(MONGO_DB_API, {
+    useUnifiedTopology: true,
+  });
+  const db = client.db("tasks");
+  const postsCollection = db.collection("tasks_saved");
+  await postsCollection.deleteOne({ id });
 
-//   client.close();
+  client.close();
 
-//   res.send(JSON.stringify("Task deleted"));
-// });
+  res.send(JSON.stringify("Task deleted"));
+});
 
-// app.put("/tasks/:id", async (req, res) => {
-//   console.log("PUT sent");
-//   const client = await MongoClient.connect(MONGO_DB_API, {
-//     useUnifiedTopology: true,
-//   });
+app.put("/tasks/:id", async (req, res) => {
+  // console.log(id);
+  const client = await MongoClient.connect(MONGO_DB_API, {
+    useUnifiedTopology: true,
+  });
 
-//   const db = client.db("tasks");
-//   const postsCollection = db.collection("tasks_saved");
+  const db = client.db("tasks");
+  const postsCollection = db.collection("tasks_saved");
 
-//   const id = req.body._id;
-//   const reminder = req.body.reminder;
+  const id = req.body._id;
+  const reminder = req.body.reminder;
 
-//   const update = {
-//     $set: { reminder },
-//   };
+  const update = {
+    $set: { reminder },
+  };
 
-//   const options = { upsert: false };
-//   await postsCollection.updateOne({ _id: ObjectId(id) }, update, options);
+  const options = { upsert: false };
+  await postsCollection.updateOne({ _id: ObjectId(id) }, update, options);
 
-//   client.close();
+  client.close();
 
-//   res.send("Updated successfully");
-// });
+  res.send("Updated successfully");
+});
 
 const port = 8080;
 app.listen(port);
